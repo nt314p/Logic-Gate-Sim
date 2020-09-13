13588e94ffb12d6a69b4b03915c9168a9477da03
Nick Tong <nicktong.001@gmail.com>
2020-09-12 22:16:54 -0400

    Started work on selection box

---
+++ Assets/Scripts/SelectionBoxController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace LogicGateSimulator
+{
+    
+    public class SelectionBoxController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform selectionBoxRect;
+        [SerializeField] private Image selectionBoxImage;
+        [SerializeField] private Camera mainCamera;
+        private Vector3 _startDragCoordinates;
+        private bool _isDragging;
+        
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            selectionBoxImage.enabled = false;
+            _isDragging = false;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var hit = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                var hitCollider = hit.collider;
+                if (hitCollider != null && !hitCollider.CompareTag("Part"))
+                {
+                    var coords = Input.mousePosition;
+                    selectionBoxImage.enabled = true;
+                    selectionBoxRect.position = coords;
+                    Debug.Log(coords);
+                }
+            }
+        }
+    }
+}
